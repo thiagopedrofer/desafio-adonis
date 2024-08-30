261c8ad51861fcf33933d06d707eb84d9e8cf20a
thiagoandrademvceditora <thiago.andrade@mvceditora.com.br>
2024-08-29 23:27:40 -0300

    test creata task

--- tests/functional/tasks/create.spec.ts
+++ tests/functional/tasks/create.spec.ts
@@ -1,6 +1,59 @@
+import User from '#models/user'
 import { test } from '@japa/runner'
 
-test.group('Tasks create', () => {
-  test('example test', async ({ assert }) => {
+test.group('Tasks create', (group) => {
+  let user: User
+
+  group.setup(async () => {
+    
+    await User.query().where('email', 'thiago@email.com').delete()
+
+    
+    user = await User.create({
+      email: 'thiago@email.com',
+      password: '123456',
+    })
+  })
+
+  test('create a new task successfully', async ({ client, assert }) => {
+    const authResponse = await client.post('/login').json({
+      email: user.email,
+      password: '123456',
+    })
+  
+    const { token } = authResponse.body()
+  
+    const payload = {
+      name: 'Desafio AdonisJs',
+      description: 'Criar teste de integração do create task',
+    }
+  
+    const response = await client
+      .post('/api/task')
+      .header('Authorization', `Bearer ${token}`)
+      .json(payload)
+  
+    response.assertStatus(201)
+    assert.isObject(response.body())
+  })
+
+  test('should return validation error when payload is invalid', async ({ client }) => {
+    const authResponse = await client.post('/login').json({
+      email: 'thiago@email.com',
+      password: '123456',
+    })
+
+    const { token } = authResponse.body()
+
+    const payload = {
+      // Dados inválidos
+    }
+
+    const response = await client
+      .post('/api/task')
+      .header('Authorization', `Bearer ${token}`)
+      .json(payload)
+
+    response.assertStatus(422)
   })
 })
